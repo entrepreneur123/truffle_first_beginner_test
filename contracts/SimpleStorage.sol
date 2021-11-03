6284d753fd3932f1bbcba91d5472f6a8d181ca54
@entrepreneur123 <irusha2056@gmail.com>
2021-11-03 10:29:38 +0545

    truffle beginner

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,15 @@
+//SPDX-Liscense-Identifier: MIT;
+
+
+pragma solidity ^0.8.9;
+
+contract SimpleStorage {
+    uint data;
+    function updateData(uint _data) external {
+        data = _data;
+    }
+
+    function readData() external view returns(uint) {
+        return data;
+    }
+}
